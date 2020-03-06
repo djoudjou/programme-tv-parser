db7ce31793f142e17063a605686aeaf9bb38daf2
M05034 <aurelien.djoutsop@bpifrance.fr>
2020-03-06 23:40:19 +0100

    init

---
+++ java/src/main/java/fr/djoutsop/pgrtv/programmetv/ProgrammeTvApplication.java
@@ -0,0 +1,13 @@
+package fr.djoutsop.pgrtv.programmetv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammeTvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammeTvApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/fr/djoutsop/pgrtv/programmetv/ProgrammeTvApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.djoutsop.pgrtv.programmetv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammeTvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
